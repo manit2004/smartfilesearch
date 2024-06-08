1844af30ae641f0d9785e314f95121526959a806
Manit Roy <138352990+manit2004@users.noreply.github.com>
2024-06-08 20:33:53 +0530

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,4 +1,4 @@
 $directory = "E:\"  # You can change this to any directory you want to scan
 $items = Get-ChildItem -Path $directory -Recurse | Select-Object FullName, Name
-# Export to a single CSV file
+# Export to a single CSV file to the current working directory
 $items | Export-Csv -Path ".\file_list.csv" -NoTypeInformation
